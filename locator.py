8c1a5d342938547ef264eb04911ecbb5853c47f6
dominika3 <dominika.bujak.1996@gmail.com>
2022-01-26 22:42:09 +0100

    test site searching

--- locator.py
+++ locator.py
@@ -5,5 +5,11 @@ class MainPageLocators(object):
     SEARCH_ELEMENT = (By.ID, 'q')
 
 
-class SearchResultsPageLocators(object):
-    LINK_LOCATOR = (By.CLASS_NAME, 'iUh30 qLRx3b tjvcx')
+class SearchPageLocators(object):
+    LINK_LOCATOR = (By.XPATH, '/html/body/div[8]/div/div[10]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/a/div/cite')
+    PERSONALIZE_SEARCH_DISABLE_BUTTON = (By.XPATH, '/html/body/c-wiz/div/div/div/div[2]/div[3]/div[2]/div/div[2]/div[1]/div/button')
+    PERSONALIZE_YOUTUBE_DISABLE_BUTTON = (By.XPATH, '/html/body/c-wiz/div/div/div/div[2]/div[4]/div[2]/div/div[2]/div[1]/div/button')
+    PERSONALIZE_ADVERTS_DISABLE_BUTTON = (By.XPATH, '/html/body/c-wiz/div/div/div/div[2]/div[5]/div[2]/div[2]/div/div[2]/div[1]/div/button')
+    PERSONAL_SETTINGS_CONFIRM_BUTTON = (By.XPATH, '/html/body/c-wiz/div/div/div/div[2]/form/div/button')
+    ADJUST_COOKIES_BUTTON = (By.ID, 'VnjCcb')
+
--- page.py
+++ page.py
@@ -1,6 +1,9 @@
+from selenium.common.exceptions import TimeoutException
+
 from element import BasePageElement
-from locator import MainPageLocators
-from locator import SearchResultsPageLocators
+from locator import SearchPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
@@ -24,23 +27,44 @@ class MainPage(BasePage):
         return "Google" == self.driver.title
 
     def is_site_search_working(self):
-        adjust_button = self.driver.find_element(By.ID, "VnjCcb")
-        adjust_button.click()
-        disable_features_buttons = self.driver.find_elements(By.CLASS_NAME, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-INsAgc VfPpkd-LgbsSe-OWXEXe-dgl2Hf Rj2Mlf OLiIxf PDpWxe P62QJc S82sre")
 
-        for button in disable_features_buttons:
+        adjust_cookies_button = self.driver.find_element(*SearchPageLocators.ADJUST_COOKIES_BUTTON)
+        adjust_cookies_button.click()
+        personalize_search_disable = self.driver.find_element(*SearchPageLocators.PERSONALIZE_SEARCH_DISABLE_BUTTON)
+        personalize_youtube_history_disable = self.driver.find_element(*SearchPageLocators.
+                                                                       PERSONALIZE_YOUTUBE_DISABLE_BUTTON)
+        personalize_adverts_disable = self.driver.find_element(*SearchPageLocators.PERSONALIZE_ADVERTS_DISABLE_BUTTON)
+        personal_settings_confirm_button = self.driver.find_element(*SearchPageLocators.PERSONAL_SETTINGS_CONFIRM_BUTTON)
+
+        cookies_buttons = [personalize_search_disable, personalize_youtube_history_disable,
+                           personalize_adverts_disable, personal_settings_confirm_button]
+
+        for button in cookies_buttons:
             button.click()
 
-        confirm_button = self.driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc XICXwf")
-        confirm_button.click()
+        search_box = self.driver.find_element(By.NAME, 'q')
+        phrase_to_be_searched = "site:wikipedia.org java"
+        search_box.click()
+        search_box.send_keys(phrase_to_be_searched)
+        search_box.send_keys(Keys.ENTER)
 
+        searched_links_on_page = []
+        for i in range(1, 10):
+            timeout = 5
+            try:
+                search_result = WebDriverWait(self.driver, timeout) \
+                    .until(EC.presence_of_all_elements_located((By.XPATH,
+                                                                "//*[@id=\"rso\"]/div[{}]/div/div[1]/div/a".format(i))))
+                links_in_search_result = [elem.get_attribute('href') for elem in search_result]
+                searched_links_on_page.append(links_in_search_result)
+            except TimeoutException:
+                print("Timeout while looking for 'href' element")
 
-        # search_element = self.driver.find_element(By.NAME, 'q')
-        # self.driver.implicitly_wait(15)
-        # search_element.click()
-        # search_text_element = "site:wikipedia.org java"
-        # search_element.send_keys(search_text_element)
-        # search_element.send_keys(Keys.ENTER)
-        # result_link = self.driver.find_element(SearchResultsPageLocators.LINK_LOCATOR)
-        # assert "wikipedia.org" in result_link
+        searched_correctly = True
+        for links_in_search_result in searched_links_on_page:
+            for link in links_in_search_result:
+                if "wikipedia.org" not in link:
+                    searched_correctly = False
+
+        return searched_correctly
 
